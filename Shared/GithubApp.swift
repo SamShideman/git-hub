0619a682a76d75e900f9c61aa5802021d408a89b
Sam Shideman <Shidsa@student.breckschool.org>
2021-05-03 14:08:02 -0500

    Initial Commit

---
+++ Shared/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Shared
+//
+//  Created by Sam Shideman on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
